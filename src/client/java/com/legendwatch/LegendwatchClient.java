457c96a4e86a2a960a5fb830591b5989dff1d75b
0utsights <outsightszs@outlook.com>
2026-04-30 17:30:02 -0400

    replace KeyBinding.Category API with string constructor to fix 1.21.11 crash

--- src/client/java/com/legendwatch/LegendwatchClient.java
+++ src/client/java/com/legendwatch/LegendwatchClient.java
@@ -8,9 +8,6 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
-//? if >=1.21.9 {
-import net.minecraft.util.Identifier;
-//?}
 import org.lwjgl.glfw.GLFW;
 
 public class LegendwatchClient implements ClientModInitializer {
@@ -25,43 +22,10 @@ public class LegendwatchClient implements ClientModInitializer {
         CraftTracker.init();
         ChatListener.init();
 
-        //? if >=1.21.9 {
-        KeyBinding.Category category = KeyBinding.Category.create(
-                Identifier.of("legendwatch", "general")
-        );
-
         toggleModKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.legendwatch.toggle_mod",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_UNKNOWN,
-                category
-        ));
-
-        toggleIconsKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.legendwatch.toggle_icons",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_UNKNOWN,
-                category
-        ));
-
-        togglePredictedKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.legendwatch.toggle_predicted",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_UNKNOWN,
-                category
-        ));
-
-        toggleTransparentKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.legendwatch.toggle_transparent",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_UNKNOWN,
-                category
-        ));
-        //?} else {
-        /*toggleModKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.legendwatch.toggle_mod",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_UNKNOWN,
                 "key.category.legendwatch.general"
         ));
 
@@ -84,8 +48,7 @@ public class LegendwatchClient implements ClientModInitializer {
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_UNKNOWN,
                 "key.category.legendwatch.general"
-        ));*/
-        //?}
+        ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (toggleModKey.wasPressed()) {
